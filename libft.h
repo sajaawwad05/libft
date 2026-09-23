959066574812fe0986735e78a9504a280d9089fe
saja <sajaawwad2005@gmail.com>
2026-09-23 22:32:46 +0300

    8 functions

---
+++ libft.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int	ft_isalpha (int	c);
+int 	ft_isdigit (int c);
+int	ft_isalnum (int c);
+int	ft_isascii (int	c);
+int	ft_isprint (int	c);
+size_t	ft_strlen (const char* s);
+int	ft_toupper (int	c);
+int	ft_tolower (int	c);
---
+++ main.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+#include <stdio.h>
+int	main(void)
+{
+	//printf("%d", ft_isalpha('k'));
+	//printf("%d\n", ft_isdigit('l'));
+	//printf("%d\n", ft_isalnum('f'));
+	//printf("%d\n", ft_isascii('5'));
+	//printf("%d\n", ft_isprint('\0'));
+	//printf("%ld\n", ft_strlen("stars88"));
+	//printf("%c\n", ft_toupper('d'));
+	printf("%c\n", ft_tolower('H'));
+
+	return (0);
+}
